e8b7acf6b7d2fd836c6c2c6a7ca49d75bfcb4890
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-10-10 14:05:58 +0200

    Run fmt
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/stream.rs
+++ src/stream.rs
@@ -2,33 +2,37 @@ use std::process;
 
 use anyhow::anyhow;
 
-use futures_util::stream::{Stream, unfold};
+use futures_util::stream::{unfold, Stream};
 
 use kobject_uevent::UEvent;
 
-use netlink_sys::{TokioSocket, SocketAddr, protocols::NETLINK_KOBJECT_UEVENT};
+use netlink_sys::{protocols::NETLINK_KOBJECT_UEVENT, SocketAddr, TokioSocket};
 
 /// creates a new stream of UEvents
-pub fn uevents() -> anyhow::Result<impl Stream<Item=anyhow::Result<UEvent>>> {
-    let mut socket =  TokioSocket::new(NETLINK_KOBJECT_UEVENT)
+pub fn uevents() -> anyhow::Result<impl Stream<Item = anyhow::Result<UEvent>>> {
+    let mut socket = TokioSocket::new(NETLINK_KOBJECT_UEVENT)
         .map_err(|e| anyhow!("Socket open error: {}", e))?;
     let sa = SocketAddr::new(process::id(), 1);
-    socket.bind(&sa)
+    socket
+        .bind(&sa)
         .map_err(|e| anyhow!("Socket bind error: {}", e))?;
 
-    Ok(unfold((socket, vec![0; 1024 * 8]), |(mut socket, mut buf)| async move {
-        let n = match socket.recv_from(&mut buf).await {
-            Ok((n, _addr)) => {
-                if n == 0 {
-                    return None;
+    Ok(unfold(
+        (socket, vec![0; 1024 * 8]),
+        |(mut socket, mut buf)| async move {
+            let n = match socket.recv_from(&mut buf).await {
+                Ok((n, _addr)) => {
+                    if n == 0 {
+                        return None;
+                    }
+                    n
                 }
-                n
-            },
-            Err(e) => {
-                return Some((Err(anyhow!("Socket receive error: {}", e)), (socket, buf)));
-            },
-        };
+                Err(e) => {
+                    return Some((Err(anyhow!("Socket receive error: {}", e)), (socket, buf)));
+                }
+            };
 
-        Some((UEvent::from_netlink_packet(&buf[0..n]), (socket, buf)))
-    }))
+            Some((UEvent::from_netlink_packet(&buf[0..n]), (socket, buf)))
+        },
+    ))
 }
